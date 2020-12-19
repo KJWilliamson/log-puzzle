58025a88a1adffaccfc1c1a17965a1fa55d5973a
Kamela Williamson <kami63116@yahoo.com>
2020-12-18 18:48:45 -0700

    created animal directory and place directory for downloads

--- logpuzzle.py
+++ logpuzzle.py
@@ -13,6 +13,10 @@ Here's what a puzzle URL looks like (spread out onto multiple lines):
 HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US;
 rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 """
+__author__ = "kamela williamson"
+# got clarification from Jalon on exactly what needed
+# to be done and how
+# worked on it with Mai & Elisia
 
 import os
 import re
@@ -21,13 +25,32 @@ import urllib.request
 import argparse
 
 
+def helper(url):
+    d_split = url.split("-")
+    return d_split[-1]
+
+
 def read_urls(filename):
     """Returns a list of the puzzle URLs from the given log file,
     extracting the hostname from the filename itself, sorting
     alphabetically in increasing order, and screening out duplicates.
     """
     # +++your code here+++
-    pass
+    final_path = []
+    with open(filename) as f:
+        for lines in f:
+            if filename == "animal_code.google.com":
+                urls = re.search(r'\S+puzzle\S+', lines)
+                if urls:
+                    final_path.append("http://code.google.com" + urls.group())
+                    results = sorted(set(final_path))
+            else:
+                urls = re.search(r"\S+puzzle\/(\w+)-(\w+)-(\w+).jpg", lines)
+                if urls:
+                    final_path.append("http://code.google.com" + urls.group())
+                    results = sorted(set(final_path), key=helper)
+
+        return results
 
 
 def download_images(img_urls, dest_dir):
@@ -39,7 +62,15 @@ def download_images(img_urls, dest_dir):
     Creates the directory if necessary.
     """
     # +++your code here+++
-    pass
+    if not os.path.isdir(dest_dir):
+        os.makedirs(dest_dir)
+    img_tag = ""
+    for i, url in enumerate(img_urls):
+        print("Retrieving...", url)
+        urllib.request.urlretrieve(url, filename=dest_dir + "/img" + str(i) + ".jpg")
+        img_tag += "<img src='img" + str(i) + ".jpg'>"
+    with open(dest_dir + "/index.html", "w") as f:
+        f.write("<html> <body>" + img_tag + "<body> <html>")
 
 
 def create_parser():
